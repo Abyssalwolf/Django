f17f2725522990d27403889e2dd4d7bd54a4542b
Sebin Thomas <sebinthomas299@gmail.com>
2024-05-10 22:58:44 +0300

    modifies url_shortener

--- URL_SHORTENER/urlshort/shortener/views.py
+++ URL_SHORTENER/urlshort/shortener/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Url
+from . forms import UrlForm
+import shortuuid
+
 
 # Create your views here.
 
+def index(request):
+    form = UrlForm(request.POST or None)
+    if form.is_valid():
+        long_url = form.cleaned_data['original']
+        custom = form.cleaned_data['custom']
+        short = shortuuid.uuid()
+        new_url = Url.objects.create(original=long_url, short=short, custom=custom)
+        return redirect('shortened_url', short_code=short)
+    context = {'form': form}
+    return render(request, 'index.html', context)
+
+def redirect_original_url(request):
+    try:
+        url_obj = Url.objects.get('short')
+        return redirect(url_obj)
+    except Url.DoesNotExist:
+        return render(request, 'error.html')
--- URL_SHORTENER/urlshort/urlshort/urls.py
+++ URL_SHORTENER/urlshort/urlshort/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shortener import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('<str:short_code>/', views.redirect_original_url, name='shortened_url'),
 ]
