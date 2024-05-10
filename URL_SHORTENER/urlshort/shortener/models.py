2abab61463f054327fe89a01d77d5b0adc1d1c06
Sebin Thomas <sebinthomas299@gmail.com>
2024-05-10 06:34:09 +0300

    modified urlshortener project

--- URL_SHORTENER/urlshort/shortener/models.py
+++ URL_SHORTENER/urlshort/shortener/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Url(models.Model):
+    original=models.CharField(max_length=500)
+    short=models.CharField(max_length=300)
+    custom=models.CharField(max_length=250)
+    count=models.IntegerField(default=0)
